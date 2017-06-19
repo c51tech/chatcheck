5ef1f8f6268665311775c070fd6780fdffb5cefc
Kwonill Kim <kikim@delcam.co.kr>
2017-06-19 18:48:18 +0900

    #3: 데이터 one-hot encoding 구현

--- comment_loader.py
+++ comment_loader.py
@@ -1,7 +1,33 @@
 #-*- coding: utf-8 -*-
 
+import math
+import os
+import random
+import re
+import numpy as np
 
-def load(file_path):
+
+def load_comments(count=1000, dir_path='.', file_filter=r'.+\.txt', count_per_file=100):
+    file_names = os.listdir(dir_path)
+
+    file_names = [f for f in file_names if re.match(file_filter, f)]
+
+    if len(file_names) == 0:
+        raise Exception('No files. dir=[%s], filter=[%s]' % (dir_path, file_filter))
+
+    num_iter = math.ceil(count / count_per_file)
+
+    comments = [None] * count
+
+    for i in range(num_iter):
+        filename = random.choice(file_names)
+
+        comments[i * count_per_file: (i+1) * count_per_file] = load(dir_path + '/' + filename, count=count_per_file)
+
+    return comments
+
+
+def load(file_path, count=100):
     with open(file_path, 'r') as data_file:
         lines = data_file.readlines()
 
@@ -25,6 +51,7 @@ def load(file_path):
                 comment += l
 
     comments = comments[:count_comments]
+    comments = random.choices(comments, k=count)
 
     return comments
 
@@ -55,7 +82,7 @@ def disassemble_str(sentence):
 
 
 def get_char2idx(chars):
-    return { ch:i for i,ch in enumerate(chars) }
+    return {ch: i for i, ch in enumerate(chars)}
 
 
 def convert_char_to_idx(chars, char2idx):
@@ -65,7 +92,7 @@ def convert_char_to_idx(chars, char2idx):
         if ch in char2idx:
             idx_list[i] = char2idx[ch]
         else:
-            idx_list[i] = -1
+            idx_list[i] = len(char2idx)
 
     return idx_list
 
@@ -76,12 +103,28 @@ std_char_list = '!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\
                 ' \t\n'
 
 
-def convert_comments2charidx(comments, char2idx):
+def convert_comments_to_charidx(comments, char2idx):
     charidx_list = [None] * len(comments)
     for i, c in enumerate(comments):
         charidx_list[i] = convert_char_to_idx(disassemble_str(c), char2idx)
 
     return charidx_list
+
+
+def convert_charidx_to_onehot(comments_charidx, num_char=146):
+
+    n = len(comments_charidx)
+    data = [None] * n
+
+    for i, c in enumerate(comments_charidx):
+        comment_mat = np.zeros([len(c), num_char])
+
+        for row, col in enumerate(c):
+            comment_mat[row, col] = 1
+
+        data[i] = comment_mat
+
+    return data
 
 
 if __name__ == "__main__":
@@ -98,7 +141,17 @@ if __name__ == "__main__":
 
     comments = load('comments_pgr_recommend_1-2_20170617_185152.txt')
 
-    comments_charidx = convert_comments2charidx(comments[:2], char2idx)
+    comments_charidx = convert_comments_to_charidx(comments[:2], char2idx)
 
     print(comments[:2])
     print(comments_charidx)
+
+    print(len(std_char_list))
+
+    data = convert_charidx_to_onehot(comments_charidx)
+
+    print(data[0][0])
+    print(data[0].shape)
+
+    print(data[1][0])
+    print(data[1].shape)
