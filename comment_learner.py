5bf728aa60b5ac96492090ebdc53890cba30c453
macromancer <kwonill.kim@gmail.com>
2017-06-26 09:44:58 +0900

    #6: transfer learning으로 오류사항 수정

--- comment_learner.py
+++ comment_learner.py
@@ -1,9 +1,11 @@
 import numpy as np
 import datetime
+import random
 import sys
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+from keras.layers import Flatten
 from keras.layers import LSTM
 from keras.models import model_from_json
 from keras.callbacks import ModelCheckpoint
@@ -19,7 +21,7 @@ import comment_loader
 char2idx = comment_loader.get_char2idx(comment_loader.std_char_list)
 
 
-def load(count, dir_path='.'):
+def load_data(count, dir_path='.'):
 
     comments_pgr = comment_loader.load_comments(count=count//2, dir_path=dir_path, file_filter=r'.+pgr.+\.txt', count_per_file=count//16)
     comments_ilbe = comment_loader.load_comments(count=count//2, dir_path=dir_path, file_filter=r'.+ilbe.+\.txt', count_per_file=count//16)
@@ -39,7 +41,7 @@ def load(count, dir_path='.'):
     return comments, y
 
 
-def preprocess_comments(comments, max_review_length=100):
+def preprocess_comments(comments, max_review_length=128):
     x = comment_loader.convert_comments_to_charidx(comments, char2idx)
     x = comment_loader.convert_charidx_to_onehot(x)
     x = sequence.pad_sequences(x, maxlen=max_review_length)
@@ -47,16 +49,36 @@ def preprocess_comments(comments, max_review_length=100):
     return x
 
 
-def learn(train_dir='.', test_dir='.', count=1024, test_count=1024, epochs=10, max_review_length=100):
+def load_model(model_file_path, weight_file_path):
+    json_file = open(model_file_path, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
 
-    model = Sequential()
-    model.add(LSTM(128, input_shape=(max_review_length, len(char2idx) + 1)))
-    model.add(Dense(1, activation='linear'))
+    model.load_weights(weight_file_path)
+
+    model.summary()
+
+    return model
+
+
+def learn(model=None, train_dir='.', test_dir='.', count=1024, test_count=1024, max_iter=100, epochs=10, max_review_length=128, data_modifier=None):
+
+    if model is None:
+        model = Sequential()
+        model.add(LSTM(256, input_shape=(max_review_length, len(char2idx) + 1), return_sequences=True))
+        model.add(Dropout(0.2))
+        model.add(LSTM(256, return_sequences=True))
+        model.add(Dropout(0.2))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(1, activation='relu'))
 
     learning_rate = 0.001
     adam = Adam(lr=learning_rate)
     model.compile(loss='mean_squared_error', optimizer=adam, metrics=['mae'])
 
+    start_time = datetime.datetime.now()
     timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 
     model_json = model.to_json()
@@ -64,63 +86,80 @@ def learn(train_dir='.', test_dir='.', count=1024, test_count=1024, epochs=10, m
         json_file.write(model_json)
 
     weight_file = './weights_%s.h5' % timestamp
-    checkpoint = ModelCheckpoint(weight_file, monitor='loss', verbose=1, save_best_only=True, mode='min')
-    callback_list = [checkpoint]
+    # checkpoint = ModelCheckpoint(weight_file, monitor='loss', verbose=1, save_best_only=False, mode='min')
+    # callback_list = [checkpoint]
 
-    for i in range(200):
+    for i in range(max_iter):
         print('################# Iteration: %d' % i)
 
-        comments_train, y_train = load(count, dir_path=train_dir)
-        comments_test, y_test = load(test_count, dir_path=test_dir)
+        comments_train, y_train = load_data(count, dir_path=train_dir)
+        comments_test, y_test = load_data(test_count, dir_path=test_dir)
+
+        if data_modifier is not None:
+            comments_train = data_modifier(comments_train, y_train)
+            comments_test = data_modifier(comments_test, y_test)
 
         x_train = preprocess_comments(comments_train, max_review_length)
         x_test = preprocess_comments(comments_test, max_review_length)
 
-        # y_train = np_utils.to_categorical(y_train)
-        # y_test = np_utils.to_categorical(y_test)
+        model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=16, verbose=2)
 
-        model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=16, callbacks=callback_list, verbose=2)
+        model.save_weights(weight_file)
 
-        # model.save_weights('model_%s.h5' % timestamp)
         K.set_value(adam.lr, learning_rate / (epochs * (i + 1)))
 
-    scores = model.evaluate(x_test, y_test, verbose=0)
-    print('Accuracy %f' % scores[1])
+    print('\nLearning Time: %s' % (datetime.datetime.now() - start_time))
+
+    comments_test, y_test, = load_data(count=test_count, dir_path='/media/kikim/Data/data/chatcheck/test')
+    x_test = preprocess_comments(comments_test, max_review_length=max_review_length)
+
+    prediction = model.predict(x_test, verbose=0)
+
+    diff = np.sum(np.abs(prediction - y_test.reshape(prediction.shape)), axis=1)
+    is_wrong = diff >= 0.5
+    count_wrong = np.sum(is_wrong)
+    print('Accuracy: %.2f%%' % (100 - count_wrong / test_count * 100))
+
+
+def comment_modifier(comments, y):
+
+    for idx, c in enumerate(comments):
+        if y[idx] == 0:
+            r = random.random()
+            if r < 0.033:
+                comments[idx] += ';' * random.randint(1, 50)
+            elif r < 0.66:
+                comments[idx] += '.' * random.randint(1, 50)
+    return comments
+
+
+def comment_mixer(comments, y):
+    comments_pgr = comments[y == 1]
+
+    for idx, c in enumerate(comments):
+        if y[idx] == 0 and random.random() < 0.1:
+            i = random.randint(0, comments_pgr.shape[0] - 1)
+            len_to_add = -1 * min(len(comments_pgr[i]), 20)
+            comments[idx] += ' ' + comments_pgr[i][len_to_add:]
+
+    return comments
 
 
 def test(model_file_path, weight_file_path, x_test, y_test, threshold=0.5):
-    json_file = open(model_file_path, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
 
-    loaded_model.load_weights(weight_file_path)
-
-    loaded_model.summary()
-
+    loaded_model = load_model(model_file_path, weight_file_path)
     loaded_model.compile(loss='mean_squared_error', optimizer='rmsprop', metrics=['mae'])
 
-    # score = loaded_model.evaluate(x_test, y_test)
-    # print('\n%s: %.2f%%' % (loaded_model.metrics_names[1], score[1] * 100))
-
     prediction = loaded_model.predict(x_test, verbose=1)
-    print(prediction.shape)
-    print(prediction[:5, :])
-    print(prediction[0, 0])
     diff = np.sum(np.abs(prediction - y_test.reshape(prediction.shape)), axis=1)
     wrong = diff >= threshold
-    print(wrong.shape)
+
     return wrong, prediction
 
 
 def play(model_file_path, weight_file_path):
-    json_file = open(model_file_path, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
 
-    loaded_model.load_weights(weight_file_path)
-
+    loaded_model = load_model(model_file_path, weight_file_path)
     loaded_model.compile(loss='mean_squared_error', optimizer='rmsprop', metrics=['mae'])
     loaded_model.summary()
 
@@ -149,10 +188,10 @@ if __name__ == "__main__":
     elif sys.argv[1] == 'test':
         count_test = 1024
         max_review_length = 128
-        comments, y_test,  = load(count=count_test, dir_path='/media/kikim/Data/data/chatcheck/test')
+        comments, y_test,  = load_data(count=count_test, dir_path='/media/kikim/Data/data/chatcheck/test')
         x_test = preprocess_comments(comments, max_review_length=max_review_length)
 
-        is_wrong, pred = test('./model_20170624_193321.json', 'weights-20170624_193321.h5', x_test, y_test, threshold=0.5)
+        is_wrong, pred = test('./model_20170626_005027.json', './weights_20170626_005027.h5', x_test, y_test, threshold=0.5)
 
         print('Accuracy: %.2f%%' % (100 - np.sum(is_wrong) / count_test * 100))
         for i in enumerate(is_wrong):
@@ -160,4 +199,16 @@ if __name__ == "__main__":
                 print('%f %f %s' % (y_test[i][0], pred[i][0], comments[i][0]))
 
     elif sys.argv[1] == 'play':
-        play('./model_20170624_193321.json', './weights-20170624_193321.h5')
+        # play('./model_20170625_132026.json', './weights_20170625_132026.h5')
+        # play('./model_20170625_212948.json', './weights_20170625_212948.h5')
+        play('./model_20170626_005027.json', './weights_20170626_005027.h5')
+
+    elif sys.argv[1] == 'transfer1':
+        model = load_model('./model_20170625_132026.json', './weights_20170625_132026.h5')
+        learn(model=model, train_dir='/media/kikim/Data/data/chatcheck', test_dir='/media/kikim/Data/data/chatcheck',
+              count=1024, test_count=128, max_iter=20, epochs=5, max_review_length=128, data_modifier=comment_modifier)
+
+    elif sys.argv[1] == 'transfer2':
+        model = load_model('./model_20170625_212948.json', './weights_20170625_212948.h5')
+        learn(model=model, train_dir='/media/kikim/Data/data/chatcheck', test_dir='/media/kikim/Data/data/chatcheck',
+              count=1024, test_count=128, max_iter=20, epochs=5, max_review_length=128, data_modifier=comment_mixer)
